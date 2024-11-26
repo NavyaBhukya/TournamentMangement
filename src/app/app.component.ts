bbecb091f05583a10b02a88bc53cf18827f8daa1
Mahesh <itstribe.mahi@gmail.com>
2024-11-26 22:02:50 +0530

    routing, auth guard and tournament flow improved

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'TMS';
+export class AppComponent implements OnInit {
+  public isLoggedIn: boolean = false;
+  ngOnInit(): void {
+    this.isLoggedIn = localStorage.getItem('token') ? true : false
+  }
+
 }
---
+++ src/app/guards/logInGuard/user-loggedin.guard.ts
@@ -0,0 +1,21 @@
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+
+export const userLoggedinGuard: CanActivateFn = (route, state) => {
+  const token = localStorage.getItem('token');
+  if (token) {
+    try { // Validate the token (e.g., JWT expiry or structure check)
+      const decodedToken = JSON.parse(atob(token.split('.')[1]));
+      const isExpired = decodedToken.exp && decodedToken.exp * 1000 < Date.now();
+      if (!isExpired) {
+        return true; // Token is valid
+      }
+    } catch (error) {
+      console.error('Invalid token:', error);
+    }
+  }
+  // Redirect to login if not authenticated
+  const router = inject(Router);
+  router.navigate(['/login']);
+  return false;
+};
